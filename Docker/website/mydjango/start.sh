422e751dc9ee5abbec52fead38fc59c4b31c5980
John <pendenquejohn@gmail.com>
2019-09-13 16:07:34 +0200

    Error

--- Docker/website/mydjango/start.sh
+++ Docker/website/mydjango/start.sh
@@ -5,7 +5,7 @@ function start_development() {
 }
 
 function start_production() {
-    gunicorn kurrikulam.wsgi --workers 4 --bind 0.0.0.0:8000 --chdir=/code/website/mydjango --log-file -
+    gunicorn mydjango.wsgi -w 4 -b 0.0.0.0:8000 --chdir=/code/website/mydjango --log-file -
 }
 
 if [ ${PRODUCTION} == "false" ]; then
