91677e5a04a1d9cee94a6c8af4aefa1f7abd3b17
John <pendenquejohn@gmail.com>
2019-09-19 17:46:41 +0200

    Test

--- tests/script.py
+++ tests/script.py
@@ -8,8 +8,21 @@ import posixpath
 import requests
 
 class Certificates:
-    def __init__(self):
-        current_path = posixpath.curdir
-        print(current_path)
+    def __init__(self, domains=[]):
+        # current_path = posixpath.curdir
+        # Path that contains the live domains
+        # to be used with letsencrypt
+        live_domains_path = '/etc/letsencrypt/live'
+        # if not self._exists(live_domains_path):
+        #     for domain in domains:
+        #         os.makedirs(os.path.join(live_domains_path, domain))
 
-certificates = Certificates()
+        os.chdir(live_domains_path)
+        os.listdir(live_domains_path)
+
+    @staticmethod
+    def _exists(path):
+        return posixpath.exists(path)
+        
+
+certificates = Certificates(domains=['nawoka.fr', 'www.nawoka.fr'])
