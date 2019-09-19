e10e2b71b150e37e7ed1eb8fda7fbfde408edfd7
John <pendenquejohn@gmail.com>
2019-09-19 17:12:45 +0200

    Tests

--- Docker/https/letsencrypt.py
+++ Docker/https/letsencrypt.py
@@ -1,59 +1,85 @@
-#!/bin/sh -c
+#!/usr/bin/python
 
-print('Hello')
+import argparse
+import os
+import pathlib
+import posixpath
 
-# import os
-# import pathlib
-# import posixpath
-# import subprocess
-# from threading import Thread
+import requests
 
-# DOMAINS = ['nawoka.fr', 'www.nawoka.fr']
+# class TLSError(Exception):
+#     def __init__(self, message=None, url=None):
+#         self.message = "Could not get content for %s" % url
 
-# RSA_KEY_SIZE = 4096
+#     def __unicode__(self):
+#         return self.message
 
-# DATA_PATH = '/data/certbot'
+#     def __repr__(self):
+#         return self.__unicode__()
 
-# EMAIL = 'contact.nawoka@gmail.com'
+class TLS:
+    def download_parameters(self, create_to='/home/tls_parameters/'):
+        urls = [
+            # "https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf",
+            "https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem"
+        ]
+        
+        for url in urls:
+            try:
+                response = requests.get(url)
+            except requests.HTTPError:
+                raise
+            else:
+                if response.status_code == 200:
+                    data = response.content
+                else:
+                    print("Could not get content for %s" % url)
+                    # raise TLSError(url=url)
+                    raise Exception()
+            
+            file_name = os.path.basename(url)
+            certificate_path = posixpath.join(create_to, file_name)
 
-# STAGING = 0
+            # Objectify the paths in order
+            # to perform other quick operations
+            # if necessary
+            paths = []
+            paths.append(pathlib.PosixPath(certificate_path))
+            
+            # Write the parameters -- ready
+            # to be used elsewhere
+            with open(certificate_path, 'wb') as f:
+                f.write(data)
+        return paths
 
-# class Requester:
-#     pass
+    @staticmethod
+    def get_file_name(url):
+        return os.path.basename(url)
 
-# class LetsEncrypt(Requester):
-#     def __init__(self, **kwargs):
-#         self.base_path = posixpath.curdir
-#         path_exists = posixpath.lexists(DATA_PATH)
+class LetsEncrypt(TLS):
+    def __init__(self, **kwargs):
+        current_path = kwargs['start_path'] or posixpath.curdir
+        tls_parameters_dir = posixpath.join(current_path, 'tls_parameters')
+        # Check if we have a folder to
+        # store the different certificates
+        certificate_path_exists = posixpath.lexists(tls_parameters_dir)
 
-#         if path_exists:
-#             # A certificate already exists
-#             # and we need to replace it
-#             print('Path not there')
+        # Create the corresponding path
+        if not certificate_path_exists:
+            os.mkdir(tls_parameters_dir)
 
-#         # Check whether the required files are
-#         # present in the folder of this path
-#         # e.g. options-ssl-nginx.conf or
-#         # ssl-dhparams.pem
-#         conf_files = [
-#             posixpath.join(DATA_PATH, 'options-ssl-nginx.conf'),
-#             posixpath.join(DATA_PATH, 'ssl-dhparams.pem')
-#         ]
+        # certificates
+        paths = self.download_parameters()
+        
+        for path in paths:
+            if path.exists():
+                print('+ Created TLS parameter in %s' % path.as_posix())
 
-#     @staticmethod
-#     def path_exists(path, return_path=False):
-#         if posixpath.lexists(path):
-#             if return_path:
-#                 return path
-#             return True
+encrypt = LetsEncrypt(start_path='/home/')
 
-#         if return_path:
-#             return path
-#         return False
+# if __name__ == "__main__":
+#     args = argparse.ArgumentParser(description='Download TLS parameters for Cerbot')
+#     args.add_argument('-p', '--start', help='Starting path to download and create the parameter directory')
+#     parsed_args = args.parse_args()
 
-# encrypt = LetsEncrypt(data_path='/data/certbot')
-
-# thread1 = Thread(target=LetsEncrypt, name="Let's Encrypt")
-# thread2 = Thread(target=Requester, name='Requester')
-# thread1.start()
-# thread2.start()
+#     encrypt = LetsEncrypt(start_path=parsed_args.start)
