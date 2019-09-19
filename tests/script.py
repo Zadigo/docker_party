4044b11d472591c629678383d5680a2112ff5c5d
John <pendenquejohn@gmail.com>
2019-09-19 13:06:32 +0200

    Test

--- tests/script.py
+++ tests/script.py
@@ -1,1 +1,13 @@
-print('test')
+#!/usr/bin/python
+
+import posixpath
+
+class Requestor:
+    pass
+
+class LetsEncrypt(Requestor):
+    def __init__(self):
+        current_path = posixpath.curdir
+        print(current_path)
+
+encrypt = LetsEncrypt()
