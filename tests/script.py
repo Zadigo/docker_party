e6689e0c4d51c48fd8dc574f0e6ba344cd00ee4a
John <pendenquejohn@gmail.com>
2019-09-19 14:01:56 +0200

    Test

--- tests/script.py
+++ tests/script.py
@@ -6,8 +6,13 @@ class Requestor:
     pass
 
 class LetsEncrypt(Requestor):
-    def __init__(self):
-        current_path = posixpath.curdir
-        print(current_path)
+    def __init__(self, **kwargs):
+        current_path = kwargs['start_path'] or posixpath.curdir
+        certificates_dir = posixpath.join(current_path, 'certificates')
+        # Check if we have a folder to
+        # store the different certificates
+        certificate_path_exists = posixpath.lexists(certificates_dir)
 
-encrypt = LetsEncrypt()
+        print(certificate_path_exists)
+
+encrypt = LetsEncrypt(start_path='/home/')
