a6fd45d57a201055762b739709a9c8378d388962
John PENDENQUE <pendenquejohn@gmail.com>
2019-12-28 10:49:27 +0100

    A script for installing docker
    A handy script for quickly installing docker on a VPS

---
+++ init-docker.sh
@@ -0,0 +1,22 @@
+#! bin/bash
+
+KERNEL_INFO=uname -s
+SYSTEM_INFO=uname -m
+COMPOSE_VERSION="1.25.0"
+URL_FOR_COMPOSE="https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$KERNEL_INFO-$SYSTEM_INFO"
+
+# Installs docker
+sudo apt-get install docker.io
+# Installs docker compose
+sudo curl -L $URL_FOR_COMPOSE -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Updates docker engine
+# apt-get upgrade docker-engine
+
+# General update
+apt-get update
+apt-get upgrade
+
+# Prints Docker version
+# echo docker version | grep "Version"
