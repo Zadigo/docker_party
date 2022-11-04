b31e3e5856cbfacf7d95bd63b031942d404479f7
John PENDENQUE <pendenquejohn@gmail.com>
2022-11-04 23:16:58 +0100

    Update init-docker.sh

--- init-docker.sh
+++ init-docker.sh
@@ -3,7 +3,7 @@
 # Installs docker
 sudo apt-get install docker.io
 # Installs docker compose
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 # docker-compose run test
@@ -54,5 +54,7 @@ sudo ufw allow https
 sudo ufw allow 22
 
 # Github
-git config --global user.name "Ubuntu"
-git config --global user.email "ubuntu@gmail.com"
+read -p "Git username:" username
+git config --global user.name $username
+read -p "Git email:" email
+git config --global user.email $email
