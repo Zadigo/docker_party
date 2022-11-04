325d1effa65e69998a8e8fee464122f4c0825b9c
John PENDENQUE <pendenquejohn@gmail.com>
2022-11-04 22:36:33 +0100

    Update init-docker.sh

--- init-docker.sh
+++ init-docker.sh
@@ -5,6 +5,7 @@ sudo apt-get install docker.io
 # Installs docker compose
 curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+# docker-compose run test
 
 # Updates docker engine
 # apt-get upgrade docker-engine
